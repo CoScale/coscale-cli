a42b84c827b2ed81d7de9f5115745b0ae255c0a5
Samuel Vandamme <samuel.vandamme@coscale.com>
2015-09-16 12:07:50 +0200

    * Improved output and added creating a symlink

--- install.sh
+++ install.sh
@@ -1,57 +1,84 @@
 #!/usr/bin/env bash
 
-# COSCALE_APPID=00248f3b-294f-4adc-94eb-33c72b1d0a95 COSCALE_TOKEN=b7672983-078d-4f3d-b8c5-731f76b0786c bash -c "$(curl -L https://raw.githubusercontent.com/CoScale/coscale-cli/feature/install-script/install.sh)"
+
+#
+# Show url after getting latest version
+# Improve output formatting
+#
 
 set -u
 set -e
 
+echo
+echo "## Checking configuration and environment"
+echo
 
-echo "Preparing to install CoScale CLI"
+if [ -z ${COSCALE_APPID+x} ] || [ -z ${COSCALE_TOKEN+x} ]; then
+    echo "### Configuration"
+    echo
+    # Check command arguments
+    if [ -z ${COSCALE_APPID+x} ]; then
+        echo "Please enter your app id:"
+        read -e COSCALE_APPID
+    fi
+    echo
 
-# Check command arguments
-if [ -z ${COSCALE_APPID+x} ]; then
-    echo "App id:"
-    read -e COSCALE_APPID
+    if [ -z ${COSCALE_TOKEN+x} ]; then
+        echo "Please enter your access token:"
+        read -e COSCALE_TOKEN
+    fi
+    echo
 fi
-
-if [ -z ${COSCALE_TOKEN+x} ]; then
-    echo "Access token:"
-    read -e COSCALE_TOKEN
-fi
-
-# Fetch latest release list from Github
-echo "Getting latest release information"
-github_data=`curl -s -L https://api.github.com/repos/CoScale/coscale-cli/releases/latest | grep "browser_download_url" | awk '{ print $2; }' | sed 's/"//g'`
 
 # Detect operation system
-echo "Detecting operation system"
+echo "### Detecting operating system"
+echo
 os=`uname -o | awk '{split($0,a,"/"); print tolower(a[2])}'`
+echo "Operation system: {{$os}}"
+echo
+
+# Fetch latest release list from Github
+echo
+echo "## Getting latest release information"
+echo
+github_data=`curl -s -L https://api.github.com/repos/CoScale/coscale-cli/releases/latest | grep "browser_download_url" | awk '{ print $2; }' | sed 's/"//g'`
 
 # Select correct release
 release=`echo "$github_data" | grep $os`
+echo "### Latest release: ${{release}}"
+
+# Start install
+echo
+echo "## Installing CoScale CLI tool"
+echo
 
 # Create dirs
-echo "Creating directories /opt/coscale/cli"
-mkdir -p /opt/coscale/cli
+echo "### Creating directories /opt/coscale/cli"
+echo
+mkdir -v -p /opt/coscale/cli
 pushd /opt/coscale/cli
+echo
 
 # Install client
-echo "Downloading client to /opt/coscale/cli/coscale-cli"
+echo "### Downloading client to /opt/coscale/cli/coscale-cli"
 curl -L "$release" > coscale-cli
-chmod +x coscale-cli
+chmod -v +x coscale-cli
+echo
 
-# Add to $PATH
-echo "Adding coscale-cli to $PATH in /root/.bashrc"
-echo "export PATH=\"${PATH}:/opt/coscale/cli\"" >> /root/.bashrc
-source /root/.bashrc
+# Create symlink from /usr/bin/coscale-cli to /opt/coscale/cli/coscale-cli
+echo "### Creating symlink"
+ln -v -S /usr/bin/coscale-cli /opt/coscale/cli/coscale-cli
+echo
 
 # Create config
-echo "Creating config"
+echo "Generating config"
 echo "{\"baseurl\":\"https://api.coscale.com\", \"appid\":\"$COSCALE_APPID\", \"accesstoken\":\"$COSCALE_TOKEN\"}" | gzip -c > /opt/coscale/cli/api.conf
+echo
 
 # Test config
 echo "Testing configuration"
 coscale-cli event list
+echo
 
 # Done
 echo "Done, you can now start using the CoScale CLI tool."
