cde51c61ffab52bd273e78fa54792a1d4ceb0b11
cristiciutea <cristian.ciutea@issco.ro>
2018-06-11 09:52:36 +0300

    update coscale-cli docker files

--- docker/clean-files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-if [ -e ./files ]; then
-	rm -Rf ./files
-fi
--- docker/conf.sh
+++ docker/conf.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export VERSION=$(curl -s -X GET https://api.github.com/repos/CoScale/coscale-cli/releases/latest | grep -Po '(?<="tag_name": ")[^"]+(?=")')
-
